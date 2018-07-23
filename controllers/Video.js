4139fa9def7fbc75f1edf1854c980033e1a55754
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-07-23 14:03:27 +0200

    Deletion + reupload of Utils & Video.js in controllers

---
+++ controllers/Video.js
@@ -0,0 +1,19 @@
+var formidable = require('formidable');
+var fs = require('fs');
+var _ = require('./Utils.js');
+
+exports.registerVideo = function(req,res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.videotoupload.path;
+        var newpath = '../data/' + files.videotoupload.name; 
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            _.response.sendSucces(req,res,'/home','Video registered succesfuly');
+        });
+    });
+}
+
+exports.viewsUploadVideo = function(req,res) {
+    res.render('addVideo.ejs');
+}
