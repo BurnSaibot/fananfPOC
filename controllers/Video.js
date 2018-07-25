49ab11bcae3f02b349067d8a2a7a14d49e5fd14c
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-07-25 16:35:34 +0200

    updt - execScript
    We are now calling the script after the user send a video on this video to generates subtitles. (added module shelljs )

--- controllers/Video.js
+++ controllers/Video.js
@@ -2,17 +2,17 @@ var formidable = require('formidable');
 var fs = require('fs');
 var _ = require('./Utils.js');
 var path = require('path');
+const shell = require('shelljs');
 
 exports.registerVideo = function(req,res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         //console.log(files.videotoupload);
         var oldpath = files.videotoupload.path;
-        var newpath = path.join(__dirname, req.session.config.filePath , 'data', 'videos', files.videotoupload.name);
-        console.log(__dirname);
-        console.log(oldpath);
-        console.log(newpath);
-
+        var filename = files.videotoupload.name;
+        var propperName = filename.trim();
+        var newpath = path.join(__dirname, req.session.config.filePath , 'data', 'videos', propperName);
+        var pathOut = path.join(__dirname, req.session.config.filePath, 'data', 'subtitles');
         fs.readFile(oldpath,function(err,data) {
             if (err) _.response.sendError(res,err,500);
             console.log('File read : ' + oldpath);
@@ -22,12 +22,15 @@ exports.registerVideo = function(req,res) {
 
                 fs.unlink(oldpath, function(err) {
                     if (err) _.response.sendError(res,err,500);
-                    _.response.sendSucces(req,res,'/home',"File uploaded & stored succesfuly, temporary file deleted")
-                })
-            })
+                    shell.exec("/bin/bash /scripts/videoToSub.sh -f srt -i " + newpath + " -o " + pathOut,{silent: false}).stdout;
+                    _.response.sendSucces(req,res,'/home',"File uploaded & stored succesfuly, temporary file deleted");
 
+                });
+            });
             
-        })
+        });
+
+        
         /*fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
             _.response.sendSucces(req,res,'/home','Video registered succesfuly');
