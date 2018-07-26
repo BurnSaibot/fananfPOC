178e7253137ba3209945ab8888d09dbab82a035e
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-07-26 16:12:14 +0200

    updt: C/videos debugging

--- controllers/Video.js
+++ controllers/Video.js
@@ -6,6 +6,7 @@ const shell = require('shelljs');
 
 exports.registerVideo = function(req,res) {
     var form = new formidable.IncomingForm();
+    //parsing the form
     form.parse(req, function (err, fields, files) {
         //console.log(files.videotoupload);
         var oldpath = files.videotoupload.path;
@@ -14,20 +15,26 @@ exports.registerVideo = function(req,res) {
         var newpath = path.join(__dirname, req.session.config.filePath , 'data', 'videos', propperName);
         var pathOut = path.join(__dirname, req.session.config.filePath, 'data', 'subtitles');
         var pathScript = path.join(__dirname, "../","scripts","videoToSub.sh");
+
+        //reading the video then launch the script to get transcription
         fs.readFile(oldpath,function(err,data) {
             if (err) _.response.sendError(res,err,500);
-            console.log('File read : ' + oldpath);
             fs.writeFile(newpath,data,function(err) {
                 if (err) _.response.sendError(res,err,500);
                 console.log("File uploaded & moved");
+
                 console.log("Subtitles should be find in : " + pathOut + "\n fomart = " + fields.format);
+                //moving the temporary file to the good place : /data/videos
                 fs.unlink(oldpath, function(err) {
                     if (err) _.response.sendError(res,err,500);
+                    // executing the script to get transcription
+
                     shell.exec("/bin/bash " + pathScript + " -f " + fields.format + " -i " + newpath + " -o " + pathOut ,{silent: false},function(code,stdout,stderr) {
                         console.log("Code: " + code);
                         if (code == 0) console.log("Succesfuly transcripted : " + propperName);
                         else console.log("Couldn't succed to transcript " + propperName);
                     }).stdout;
+                    //even if the transcription isn't over, we redirect the user to home as the transcription could take a great amount of time.
                     _.response.sendSucces(req,res,'/home',"Succesfuly send the video to the server, waiting to get the transcription to generate subtitles.");
                     
 
