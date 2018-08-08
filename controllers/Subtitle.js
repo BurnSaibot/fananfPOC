3ca5fba015d8512d0ad2d646e3a09a6857342de4
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-08-08 11:00:10 +0200

    Refactoring

--- controllers/Subtitle.js
+++ controllers/Subtitle.js
@@ -6,7 +6,7 @@ var mSubtitle = require('../models/Subtitle');
 
 var extract = exports.extract = function(sub) {
     return new Promise(function(resolve,reject) {
-        fs.readFile(sub.urlSousTitres,function(err,data){
+        fs.readFile(sub.urlSousTitres,'utf-8',function(err,data){
             if (err) reject(err);
             resolve(data)
         })
@@ -16,16 +16,13 @@ var extract = exports.extract = function(sub) {
 }
 
 exports.test = function(req,res,next) {
-    mSubtitle.findById(req.params.id,function(err,result) {
-        console.log(result);
-        if (err) _.response.sendError(res,err,500);
-        extract(result)
-        .then(function(data){
-            console.log(data);
-        }).catch(function(err){
-            _.response.sendError(res,err,500);
-        })
-
+    mSubtitle.findById(req.params.id)
+    .then(extract(result))
+    .then(function(data){
+        console.log(data);
+        res.redirect('/home');
+    }).catch(function(err){
+        _.response.sendError(res,err,500);
     })
     
 }
