69f5d2a3cf809de0c4a05e587fc77cd5c695803c
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-08-08 15:53:46 +0200

    Using recursive fonction

--- controllers/Subtitle.js
+++ controllers/Subtitle.js
@@ -6,56 +6,49 @@ var path = require('path');
 var mSubtitle = require('../models/Subtitle');
 
 var extract = exports.extract = function(sub) {
-    const regNumber = "^\d{1,}$";
-    const regTimecode = "((\d{2}:){2}\d{2},\d{3})";
-    const regNonEmpty = "^\w";
+    
     return new Promise(function(resolve,reject) {
+
         fs.readFile(sub.urlSousTitres,'utf-8',function(err,data){
-            
             if (err) reject(err);
+            const regNumber = "^\d{1,}$";
+            const regTimecode = "((\d{2}:){2}\d{2},\d{3})";
+            const regNonEmpty = "^\w";
             //on séparer chaque ligne du fichier de sous-titres
             var content = data.split("\n");
             var index = [];
             var timecode = [];
             var subs = [];
-
-            var i=-1;
-
-            var loop = function(content,index,timecode,subs){
-                
-                i++;
-
-                if(i<content.length) {
-                    if (content[i].test(regNumber)) {
-
-                        index.push(content[i]);
-                    } else if ( content[i].test(regTimecode)){
-
-                        timecode.push(content[i]);
-                    } else if ( content[i].test(regNonEmpty)) {
-
-                        subs.push(content[i]);
-                    }
-                } else {
-
-                    resolve(index,timecode,subs);
-                }
-            }
-
-            loop();
+            return fillup(0,content,index,timecode,subs);
         });   
     });
 }
 
+var fillup = function(i,content,index,timecode,subs) {
+        return new Promise(function(resolve,reject){
+            if (i<content.length) {
+                if (content[i].test(regNumber)) {
+                    index.push(content[i]);
+                } else if ( content[i].test(regTimecode)){
+                    timecode.push(content[i]);
+                } else if ( content[i].test(regNonEmpty)) {
+                    subs.push(content[i]);
+                }
+                fillup(i+1,content,index,timecode,subs);
+        } else {
+            console.log("Index : \n" + index);
+            console.log("Timecode : \n" + timecode);
+            console.log("Contenu : \n" + content);
+            resolve(index,timecode,subs);
+        }
+    });
+}
 exports.test = function(req,res,next) {
     mSubtitle.findById(req.params.id)
     .then(function(result){
         return extract(result);
     })
     .then(function(index,timecode,content){
-        console.log("Index : \n" + index);
-        console.log("Timecode : \n" + timecode);
-        console.log("Contenu : \n" + content);
         res.redirect('/home');
     }).catch(function(err){
         _.response.sendError(res,err,500);
