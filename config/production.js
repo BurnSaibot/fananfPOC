95a0e5968b492d672a414e7184f23900512dedec
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-08-21 10:47:15 +0200

    Edited confi file

--- config/production.js
+++ config/production.js
@@ -1,13 +1,9 @@
 var config = {
     ubuntu: {
-        mode: 'ubuntu',
+        mode: 'production',
         db: 'mongodb://localhost:27017/fananfdb',
         filePath: "../../../",  
         port: 3000
     }
 }
 module.exports = config.ubuntu
-
-/*module.exports = function(mode) {
-    return config[mode || process.argv[2] || 'windows'] || config.windows;
-}*/
--- config/test.js
+++ config/test.js
@@ -1,6 +1,6 @@
 var config = {
     windows: {
-        mode: 'windows',
+        mode: 'tests',
         db: 'mongodb://KiwiLeOazo:Kiwi123.@ds117156.mlab.com:17156/fananfdb',
         filePath: "../../",
         port: 3000
@@ -9,6 +9,3 @@ var config = {
 
 module.exports = config.windows
 
-/*module.exports = function(mode) {
-    return config[mode || process.argv[2] || 'windows'] || config.windows;
-}*/
