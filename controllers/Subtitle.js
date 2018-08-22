5c77ed6804f437702a34843bbaf0d6c4e1249fdd
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-08-22 12:39:01 +0200

    ....

--- controllers/Subtitle.js
+++ controllers/Subtitle.js
@@ -139,7 +139,7 @@ exports.getVideoURL = function(sub_id){
             return mTranscription.findById(sub.transcription);
         })
         .then(function(transcription){
-            console.log("Dans get Video Url devrais retourner l'url :)")
+            console.log("Dans get Video Url devrais retourner l'url :transcription.urlVideo")
             resolve(transcription.urlVideo) 
         })
         .catch(function(err){
